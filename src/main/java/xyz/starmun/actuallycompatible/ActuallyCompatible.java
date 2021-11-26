6cb789223220e85d24f277028bcb3909c0408e73
muqsit <abdemuqsit@hotmail.com>
2021-11-26 15:45:20 +0500

    Initial Configurations

---
+++ src/main/java/xyz/starmun/actuallycompatible/ActuallyCompatible.java
@@ -0,0 +1,13 @@
+package xyz.starmun.actuallycompatible;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(ActuallyCompatible.MOD_ID)
+public class ActuallyCompatible {
+
+    public static final String MOD_ID = "actuallycompatible";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+}
