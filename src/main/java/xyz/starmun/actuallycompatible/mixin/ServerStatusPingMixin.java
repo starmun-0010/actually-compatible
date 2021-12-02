fd9d4e63d29c0cdcf2a2dec2d1d6fcead520a6a8
muqsit <abdemuqsit@hotmail.com>
2021-12-02 22:59:37 +0500

    Port to 1.18

--- src/main/java/xyz/starmun/actuallycompatible/mixin/ServerStatusPingMixin.java
+++ src/main/java/xyz/starmun/actuallycompatible/mixin/ServerStatusPingMixin.java
@@ -4,7 +4,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonSerializationContext;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.fmllegacy.network.FMLStatusPing;
+import net.minecraftforge.network.ServerStatusPing;
 import org.apache.commons.lang3.tuple.Pair;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,8 +15,8 @@ import xyz.starmun.actuallycompatible.ActuallyCompatible;
 import xyz.starmun.actuallycompatible.contracts.IFMLStatPingExtensions;
 import java.util.Map;
 
-@Mixin(value = FMLStatusPing.class, remap = false)
-public class FMLStatusPingMixin implements IFMLStatPingExtensions {
+@Mixin(value = ServerStatusPing.class, remap = false)
+public class ServerStatusPingMixin implements IFMLStatPingExtensions {
 
     private final static int CHANNEL_TRUNCATE_LIMIT=Integer.MAX_VALUE;
 
@@ -43,10 +43,10 @@ public class FMLStatusPingMixin implements IFMLStatPingExtensions {
         return fmlNetworkVer;
     }
 
-    @Mixin(value = FMLStatusPing.Serializer.class, remap = false)
+    @Mixin(value = ServerStatusPing.Serializer.class, remap = false)
     public static class SerializerMixin {
         @Inject(method = "serialize",at = @At("HEAD"), cancellable = true)
-        private static void serialize(FMLStatusPing forgeData, JsonSerializationContext ctx, CallbackInfoReturnable<JsonObject> cir){
+        private static void serialize(ServerStatusPing forgeData, JsonSerializationContext ctx, CallbackInfoReturnable<JsonObject> cir){
 
             JsonObject obj = new JsonObject();
             JsonArray channels = new JsonArray();
